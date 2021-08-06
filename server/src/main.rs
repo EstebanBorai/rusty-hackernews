bd6489363ad2f7122e0f7eaf80861556b36e676a
Esteban Borai <estebanborai@gmail.com>
2021-08-05 22:23:52 -0400

    fix(server): use custom CORS headers

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,10 @@
 mod app_data;
 mod error;
-mod middleware;
 mod routes;
 mod services;
 
-use actix_web::{App, HttpServer};
+use actix_web::{dev::Service, http::HeaderValue, App, HttpServer};
+use reqwest::header;
 
 use self::app_data::AppData;
 
@@ -14,12 +14,32 @@ async fn main() -> std::io::Result<()> {
     let data = AppData::new().await;
 
     HttpServer::new(move || {
-        let cors = middleware::cors::make_cors_middleware();
-
         App::new()
-            .wrap(cors)
             .app_data(data.clone())
             .configure(routes::bind_routes)
+            .wrap_fn(|req, srv| {
+                let fut = srv.call(req);
+                async {
+                    let mut res = fut.await?;
+                    let headers = res.headers_mut();
+
+                    headers.insert(
+                        header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                        HeaderValue::from_static("*"),
+                    );
+
+                    headers.insert(
+                        header::ACCESS_CONTROL_ALLOW_METHODS,
+                        HeaderValue::from_static("GET,OPTIONS"),
+                    );
+
+                    headers.insert(
+                        header::ACCESS_CONTROL_ALLOW_HEADERS,
+                        HeaderValue::from_static("*"),
+                    );
+                    Ok(res)
+                }
+            })
     })
     .bind(format!("0.0.0.0:{}", port))?
     .run()
